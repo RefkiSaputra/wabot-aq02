c6129ae076193760baf2ad646b295452ed657bd7
RefkiSaputra <89155228+RefkiSaputra@users.noreply.github.com>
2021-10-05 11:16:42 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
-  // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '6289647417373', 'Bintangp02', m)
+  // F this.sendContact(m.chat, '6285832024639', 'Nurutomo', m)
+  this.sendContact(m.chat, '6285832024639', 'Im3', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
